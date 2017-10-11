8bd08f91bf82160af97c51993cade291769d0264
frankyshtein <>
2017-10-11 22:55:56 +0300

    first implementation of slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     $("#container").fadeIn(2000).css("display","flex");
-})
+});
+
+function right () {
+    $(".block .slides img:nth-child(5)").on('click', function () {
+        $(".block .slides").fadeOut(300,function () {
+            $(".block .slides img:nth-child(1)").appendTo($(".block .slides"));
+            $(".block .slides").fadeIn(300,right());
+        })
+            console.log(1);
+});
+};
+right();

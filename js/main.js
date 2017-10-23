921c987675716778d1792154c3e93ee07996f5b2
frankyshtein <>
2017-10-23 23:00:10 +0300

    slider 3.1

--- js/main.js
+++ js/main.js
@@ -6,28 +6,30 @@ $(document).ready(function () {
 //    |
 //   \|/
 var slides = $(".block .slider .slides img");
-var middlePoint = slides.length/2|0;
-slides.slice(0,middlePoint).clone().prependTo($(".block .slider .slides"));
-slides.slice(middlePoint+1,slides.length).clone().appendTo($(".block .slider .slides"));
+var middlePoint = slides.length / 2 | 0;
+slides.slice(0, middlePoint).clone().prependTo($(".block .slider .slides"));
+slides.slice(middlePoint + 1, slides.length).clone().appendTo($(".block .slider .slides"));
 slides = $(".block .slider .slides img");
-middlePoint = slides.length/2|0;
+middlePoint = slides.length / 2 | 0;
 console.log(middlePoint);
-$(".block .slides img:nth-child("+(middlePoint+1)+")").css("box-shadow","none");
+$(".block .slides img:nth-child(" + (middlePoint + 1) + ")").css("box-shadow", "none");
+
 function slide() {
     slides = $(".block .slider .slides img");
-    middlePoint = slides.length/2|0;
+    middlePoint = slides.length / 2 | 0;
     var steps = middlePoint - $(this).index();
-    for(i = 0; i < Math.abs(steps); i++){
-        if(steps > 0) {
-            $(".block .slides").animate({left:"-=15.84%"},400)
-
-
-        }
-
+    console.log(steps);
+    if (steps > 0) {
+        $(".block .slides").animate({
+            left: (-50+15.84 * steps) + "%"
+        }, 400);
+        console.log((-50-15.84 * steps));
+    }else {
+        $(".block .slides").animate({left: (-50+15.84 * steps) + "%"}, 400);
     }
-    $(".block .slides img:nth-child("+middlePoint+")").css("box-shadow","none");
+    $(".block .slides img:nth-child(" + middlePoint + ")").css("box-shadow", "none");
 }
-$(".block .slider .slides img").on('click',slide);
+$(".block .slider .slides img").on('click', slide);
 
 // /|\
 //  |
